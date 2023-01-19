4121df4aec1ba8469725ae774483e1a74151c4b2
Nick Maslov <kategorychnost@gmail.com>
2023-01-18 22:22:46 -0500

    structuring the app, adding assets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import shuffle from './utilities/shuffle';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cards, setCards] = useState(shuffle);
+    return <div className='grid'></div>;
 }
 
 export default App;
